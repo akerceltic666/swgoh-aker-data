39569d58094a9f383634bc0d942da8aa7f8f4266
Jesus Merino <jmerino@enac.es>
2026-07-30 12:41:30 +0200

    Actualización automática de datos

--- config.js
+++ config.js
@@ -5,7 +5,12 @@ module.exports = {
         {
             name: "Loc_SPA_XM.txt.json.br",
             url: "https://raw.githubusercontent.com/swgoh-utils/gamedata/main/Loc_SPA_XM.txt.json.br"
-        }
+        },
+
+        {
+            name: "units.txt.json.br",
+            url: "https://raw.githubusercontent.com/swgoh-utils/gamedata/main/units.json.br"
+        },
 
     ],
 
